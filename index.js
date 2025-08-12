c47676b23748ba6e5c96133ff0ea645907f07a7b
bidex327 <adebayobidemi303@gmail.com>
2025-08-12 16:56:37 +0100

    Updated

--- index.js
+++ index.js
@@ -2,26 +2,35 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
 const mongoose = require("mongoose");
-
 const cors = require ('cors')
-
 require("dotenv").config();
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Alloww-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin-x-requested-with, Content-Type,Accept, Authorization;"
+    )
+    next()
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 mongoose .connect(process.env.MONGO_URI).then(() => {
     console.log("database connected successfully!!");
   }).catch((err) => {
     console.error(`an error occured:${err.message}`);
   });
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
 
 app.use('/api', userRoutes);
 
-app.get("/info", (req, res) => {
-  console.log(`made a ${req.method} to ${req.url}`);
-  res.send("this is information page!!");
-});
+app.get('/', (req, res)=>{
+res.send ('welcome')
+})
 
 app.listen(5000, () => {
   console.log("server is running on port 5000");
