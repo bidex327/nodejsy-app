4cb61a559329a860b4ff6f14b2cc27b76281db91
bidex327 <adebayobidemi303@gmail.com>
2025-08-16 10:45:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get("/", (req, res) => {
   res.send("welcome to my Api");
 });
 
-if(process.env.NODE_ENV !== 'production'){
+if(process.env.MONGO_URI !== 'production'){
     const PORT = 5000
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`)
