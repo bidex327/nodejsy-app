85fc76d0afe901754424e7497b6eaaa25c8de6c3
bidex327 <adebayobidemi303@gmail.com>
2025-09-25 12:58:50 +0100

    Updated

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 
-const cors = require("cors");
-app.use(cors());
-require("dotenv").config();
-
-const {dbConnect} = require('./lib/dbConnect')
+const { dbConnect } = require("./lib/dbConnect");
 const connectDB = async (req, res, next) => {
-    try {
-      await dbConnect();
-      next();
-    } catch (error) {
-      console.error('Database connection failed:', error);
-      res.status(503).json({ message: "Service Unavailable: Could not connect to the database." });
-    }
-  };
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  try {
+    await dbConnect();
+    next();
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    res
+    .status(503)
+    .json({
+      message: "Service Unavailable: Could not connect to the database.",
+    });
+  }
+};
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Alloww-Origin", "*");
@@ -29,16 +32,16 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use("/api", connectDB, userRoutes)
+app.use("/api", connectDB, userRoutes);
 app.get("/", (req, res) => {
   res.send("welcome to my Api");
 });
 
-if(process.env.NODE_ENV !== 'production'){
-    const PORT = 5000
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
+if (process.env.NODE_ENV !== "production") {
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }
 
 module.exports = app;
