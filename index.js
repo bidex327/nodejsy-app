6724a7c0d0c3f640fcfb293d0766fbba75584eda
bidex327 <adebayobidemi303@gmail.com>
2026-02-23 11:18:47 +0100

    Update

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ app.use(cors({
   credentials: true
 }));
 
+
+
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
